a18ef8c56b594360e863b420292010046420c6ef
Ramya <ramya@mapbox.com>
2016-06-14 17:39:37 +0530

    from feature construct

--- index.js
+++ index.js
@@ -2,24 +2,56 @@ var fs = require('fs');
 // fetch the input geojson
 var obj = JSON.parse(fs.readFileSync('./tr-test.geojson', 'utf8'));
 var relID = []; // array to store unique relation ID
+var fromFeatureJSON;
+var relGroup ={
+ 'type': 'FeatureCollection',
+ 'features': []
+ };
+
+
+function constructJSON(fromFeature){
+ var featureJSON = {
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+      "coordinates": [],
+      "type": "LineString"
+   }
+
+ };
+
+ featureJSON.geometry.coordinates.push(fromFeature.geometry.coordinates);
+ return featureJSON;
+}
 
 // iterate through geojson to store unique relation ID
 
 obj.features.forEach(function(item){
 	
- if(item.properties["@relations"]!==undefined){
- 	for(i=0; i<item.properties["@relations"].length; i++)
- 	{
- 	  if (relID.indexOf(item.properties["@relations"][i].rel) < 0)
-		{
-		  relID.push(item.properties["@relations"][i].rel);
+if(item.properties["@relations"]!==undefined){
+ for(i=0; i<item.properties["@relations"].length; i++)
+ {
+   if (relID.indexOf(item.properties["@relations"][i].rel) < 0)
+   {
+     relID.push(item.properties["@relations"][i].rel);
+     if(relID.indexOf(item.properties["@relations"][i].role == 'from')){
+     	fromFeatureJSON = constructJSON(item);
+     	relGroup.features.push(fromFeatureJSON);
+       
+       
+
+
+
+     }
 					 			
-		}
- 	}
- 	
+	}
  }
+ 	
+}
 
 });
+
+console.log(JSON.stringify(relGroup));
 
 relID.forEach(function(item){
 
