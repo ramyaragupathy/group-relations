05e6f8723ead66e64020e2c236bdda5acf710588
Ramya <ramya@mapbox.com>
2016-06-28 18:55:06 +0530

    rewrite feature structure

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 var fs = require('fs');
 // fetch the input geojson
-var data = JSON.parse(fs.readFileSync('./conditionalTR.geojson', 'utf8'));
+var data = JSON.parse(fs.readFileSync('./grpTest.geojson', 'utf8'));
 var relID = []; // array to store unique relation ID
 var fromFeatureJSON;
+// var roleColor = { 'roles': {'from':'#FF5733','to': '#FFC300','via': '#DAF7A6'}};
 var relGroup ={
  'type': 'FeatureCollection',
  'features': []
  };
 
-function constructJSON(fromFeature, trType, trID){
-   var featureJSON = {
-    // "type": "Feature",
-    // "id":"",
-    // "properties": {},
-    // "geometry": {}
-   };
+function setProperties(copyTo, copyFrom, relInfo){
+  // copyTo.properties["color"] = roleColor.roles[relInfo.role];
+  copyTo.id = copyFrom.id;
+  copyTo.properties["label"] = relInfo.role;
+  // copyTo.properties["relID"] = relInfo.rel;
+  copyTo.geometry.coordinates = copyFrom.geometry.coordinates;
+  copyTo.geometry.type = copyFrom.geometry.type;
+  return copyTo;
+}
 
-   // featureJSON.geometry.coordinates.push(fromFeature.geometry.coordinates);
-   featureJSON = fromFeature;
-   // featureJSON.properties = fromFeature.properties;
-   // featureJSON.geometry = fromFeature.geometry;
-   featureJSON.properties["TRtype"] = trType;
-   featureJSON.properties["relID"] = trID;
-   featureJSON.properties["relations"] =[];
-   return featureJSON;
+function constructJSON(fromFeature, relDetail){
+
+   var featureJSON;
+
+   if(relDetail.role === 'from'){
+   
+    featureJSON = fromFeature;
+    // featureJSON.properties["color"] = roleColor.roles[relInfo.role];
+    featureJSON.properties["TRtype"] = relDetail.reltags["restriction"];
+    featureJSON.properties["label"] = relDetail.role;
+    featureJSON.properties["relID"] = relDetail.rel;
+    featureJSON.properties["relations"] =[];
+    
+   }
+   else{
+    featureJSON = {
+    "type": "Feature",
+    "id": "",
+    "properties": {},
+    "geometry": {
+     "coordinates": [],
+     "type": ""
+     }
+    };
+    if (relDetail.role === 'to'){
+
+    featureJSON = setProperties(featureJSON, fromFeature, relDetail);
+    }
+    else if (relDetail.role === 'via'){
+    
+    featureJSON = setProperties(featureJSON, fromFeature, relDetail);
+
+    }
+
+   }
+
+  return featureJSON;
   
 
 }
@@ -31,14 +63,16 @@ function constructJSON(fromFeature, trType, trID){
 // iterate through geojson to store unique relation ID
 
 data.features.forEach(function(item){
+  var i;
+  
   if(item.properties["@relations"] !== undefined){
-    var i;
+
     for(i=0; i<item.properties["@relations"].length; i++){
       if (item.properties["@relations"][i].role === 'from')
       {
         if(relID.indexOf(item.properties["@relations"][i].rel) < 0){
              relID.push(item.properties["@relations"][i].rel);
-             fromFeatureJSON = constructJSON(item, item.properties["@relations"][i].reltags["restriction"], item.properties["@relations"][i].rel);
+             fromFeatureJSON = constructJSON(item, item.properties["@relations"][i]);
              relGroup.features.push(fromFeatureJSON);
            }
            else{
@@ -51,12 +85,13 @@ data.features.forEach(function(item){
       {
         relGroup.features.forEach(function(relItem){
           if(item.properties["@relations"][i].rel === relItem.properties.relID){
-              
-            //push the object to 'relations' property of the 'from' feature
-            relItem.properties.relations.push(JSON.parse(JSON.stringify(item)));
-            }
 
-        });
+            fromFeatureJSON = constructJSON(item, item.properties["@relations"][i]);
+            //push the object to 'relations' property of the 'from' feature
+            // relItem.properties.relations.push(JSON.parse(JSON.stringify(item)));
+            relItem.properties.relations.push(fromFeatureJSON);
+            }
+         });
       }
 
     }
@@ -65,6 +100,12 @@ data.features.forEach(function(item){
 
 });
 
+
+relGroup.features.forEach(function(eachRel){
+  delete eachRel.properties["@relations"];
+  delete eachRel.properties["@id"];
+
+});
 // console.log("relID array length: "+ relID.length);
 // console.log("relGroup array length: "+ relGroup.features.length);
 
