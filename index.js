a8d01e1177d07b50a8fe4d6d4d4a8f662537c439
Ramya <ramya@mapbox.com>
2016-06-28 12:39:26 +0530

    duplicates removal

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 // fetch the input geojson
-var data = JSON.parse(fs.readFileSync('./tr-test.geojson', 'utf8'));
+var data = JSON.parse(fs.readFileSync('./conditionalTR.geojson', 'utf8'));
 var relID = []; // array to store unique relation ID
 var fromFeatureJSON;
 var relGroup ={
@@ -8,69 +8,66 @@ var relGroup ={
  'features': []
  };
 
-
 function constructJSON(fromFeature, trType, trID){
- var featureJSON = {
-  "type": "Feature",
-  "properties": {},
-  "geometry": {
-      "coordinates": [],
-      "type": "LineString"
-   }
+   var featureJSON = {
+    // "type": "Feature",
+    // "id":"",
+    // "properties": {},
+    // "geometry": {}
+   };
 
- };
+   // featureJSON.geometry.coordinates.push(fromFeature.geometry.coordinates);
+   featureJSON = fromFeature;
+   // featureJSON.properties = fromFeature.properties;
+   // featureJSON.geometry = fromFeature.geometry;
+   featureJSON.properties["TRtype"] = trType;
+   featureJSON.properties["relID"] = trID;
+   featureJSON.properties["relations"] =[];
+   return featureJSON;
+  
 
- featureJSON.geometry.coordinates.push(fromFeature.geometry.coordinates);
- featureJSON.properties = fromFeature.properties;
- featureJSON.properties["TRtype"] = trType;
- featureJSON.properties["relID"] = trID;
- featureJSON.properties["relations"] =[];
- return featureJSON;
 }
 
 // iterate through geojson to store unique relation ID
 
 data.features.forEach(function(item){
-	
-if(item.properties["@relations"]!==undefined){
- for(i=0; i<item.properties["@relations"].length; i++)
- {
-   if (relID.indexOf(item.properties["@relations"][i].rel) < 0)
-   {
-   	 // if the relation ID is unique, store it to an array and check for the role
-     relID.push(item.properties["@relations"][i].rel);
-     if(relID.indexOf(item.properties["@relations"][i].role == 'from')){
-     	//construct a feature for 'from' object and push it to the relation Group
-     	fromFeatureJSON = constructJSON(item, item.properties["@relations"][i].reltags["restriction"], item.properties["@relations"][i].rel);
-     	relGroup.features.push(item);
+  if(item.properties["@relations"] !== undefined){
+    var i;
+    for(i=0; i<item.properties["@relations"].length; i++){
+      if (item.properties["@relations"][i].role === 'from')
+      {
+        if(relID.indexOf(item.properties["@relations"][i].rel) < 0){
+             relID.push(item.properties["@relations"][i].rel);
+             fromFeatureJSON = constructJSON(item, item.properties["@relations"][i].reltags["restriction"], item.properties["@relations"][i].rel);
+             relGroup.features.push(fromFeatureJSON);
+           }
+           else{
+           
+           }
 
 
       }
-     else{
+      else if(item.properties["@relations"][i].role !== 'from')
+      {
+        relGroup.features.forEach(function(relItem){
+          if(item.properties["@relations"][i].rel === relItem.properties.relID){
+              
+            //push the object to 'relations' property of the 'from' feature
+            relItem.properties.relations.push(JSON.parse(JSON.stringify(item)));
+            }
 
-     }
-					 			
-	}
-   else // if the relation ID is not unique
-   {
-		//iterate through the relation Group to find the matching 'from' feature
-		relGroup.features.forEach(function(relItem){
+        });
+      }
 
-		 if(item.properties["@relations"][i].rel == relItem.properties.relID){
-		 	//push the object to 'relations' property of the 'from' feature
-		 	relItem.properties.relations.push(JSON.parse(JSON.stringify(item)));
+    }
+  }
 
-		 }
-
-		});
-	}
- }
- 	
-}
 
 });
+
+// console.log("relID array length: "+ relID.length);
+// console.log("relGroup array length: "+ relGroup.features.length);
 
 console.log(JSON.stringify(relGroup));
 
 
-// console.log(relID);
